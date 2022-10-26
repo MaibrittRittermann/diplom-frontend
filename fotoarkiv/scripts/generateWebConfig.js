0dd8f90733eac14e0cfaeecc47e1ec61d535ed0b
Maibritt Rittermann <48788546+MaibrittRittermann@users.noreply.github.com>
2022-10-26 17:05:01 +0200

    Test opdt.

--- fotoarkiv/scripts/generateWebConfig.js
+++ fotoarkiv/scripts/generateWebConfig.js
@@ -1,36 +1,18 @@
-// Writes a file to src/config.js exporting the baseUrl property
-const config = require('../config');
+// Writes a file to src/config.js exporting the environment variables
+const environment = require('../environment');
 const fs = require('fs');
 const path = require('path');
 const {
-  jwtPrivateKey,
-  connectionString,
-  GOOGLE_APPLICATION_CREDENTIALS,
-  env,
-  GPC_ENDPOINT_ID,
-  GPC_DATASET_ID,
-  GPC_MODEL_NAME,
-  GPC_PROJECT_ID,
-  GPC_LOCATION
-} = config;
+  REACT_APP_API_URL,
+  PORT,
+  env
+} = environment;
 
-const template = `const Diplom_jwtPrivateKey = '${jwtPrivateKey}';
-const connectionString = '${connectionString}';
-const GOOGLE_APPLICATION_CREDENTIALS = ${GOOGLE_APPLICATION_CREDENTIALS};
-const GPC_ENDPOINT_ID = ${GPC_ENDPOINT_ID};
-const GPC_DATASET_ID = '${GPC_DATASET_ID}';
-const GPC_MODEL_NAME = '${GPC_MODEL_NAME}';
-const GPC_PROJECT_ID = '${GPC_PROJECT_ID}';
-const GPC_LOCATION = '${GPC_LOCATION}';
+const template = `const REACT_APP_API_URL = "${REACT_APP_API_URL}";
+const PORT = ${PORT};
 export {
-  baseUrl,
-  jwtPrivateKey,
-  connectionString,
-  GPC_ENDPOINT_ID,
-  GPC_DATASET_ID,
-  GPC_MODEL_NAME,
-  GPC_PROJECT_ID,
-  GPC_LOCATION
+  REACT_APP_API_URL,
+  PORT
 };`;
 const filePath = path.join(process.cwd(), 'src/config.js');
 
